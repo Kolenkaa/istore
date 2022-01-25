2b22c98a5c8108f7db33259c74957cf8f0be9989
Kolenkaa <kkolenkaa@gmail.com>
2022-01-25 12:50:46 +0300

    Make remove.classList for burger and mobileContacts

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
 	const burger = document.querySelector(".burger");
 	const mobileMenu = document.querySelector(".mobile-menu--navigation");
 	const mobilePhoneIcons = document.querySelector(".mobile-menu--phoneicons");
+	const mobileContacts = document.querySelector(".header__top");
 
 	burger.onclick = function (event) {
 		burger.classList.toggle("active");
 		mobileMenu.classList.toggle("active");
+		mobilePhoneIcons.classList.remove("active");
+		mobileContacts.classList.remove("active");
 	};
 
 	mobilePhoneIcons.onclick = function (event) {
 		mobilePhoneIcons.classList.toggle("active");
+		mobileContacts.classList.toggle("active");
+		burger.classList.remove("active");
+		mobileMenu.classList.remove("active");
 	};
 
 });
